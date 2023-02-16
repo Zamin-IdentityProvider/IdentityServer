f0361b0f1c19b74cd653c8fa6cd9f92e5db41eaa
Amirhossein Tayyar <AmirHossein.Tayyar@outlook.com>
2023-02-16 13:10:49 +0330

    init project

---
+++ IdentityServer.Endpoints.Ui/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,9 @@
+
+namespace IdentityServer.Endpoints.Ui.Pages.Logout
+{
+    public class LogoutOptions
+    {
+        public static bool ShowLogoutPrompt = true;
+        public static bool AutomaticRedirectAfterSignOut = false;
+    }
+}
